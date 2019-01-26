5184a439373e6ea45ef23b3f8c3296309220584f
kayitare cynthia <cyntkayitare@gmail.com>
2019-01-26 07:34:36 +0200

    making a title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` 
+  <h1>RECIT</h1>
+  <p>gweiugeksaais{{quote}}</p>
+  ` 
 })
 export class AppComponent {
-  title = 'project';
+  quote = 'byose bihira abakunda Imana';
 }
