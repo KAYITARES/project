8bb747cde8b44a20ae81b50e32e345cfcd220c9c
kayitare cynthia <cyntkayitare@gmail.com>
2019-01-28 09:20:33 +0200

    adding author in quote-details.component.html

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showAuthor:boolean;
-    constructor(public title:string,public quote:string,public author:string,public upvote:string,public completeDate:Date){
+    constructor(public title:string,public quotes:string,public author:string,public upvote:string,public completeDate:Date){
         this.showAuthor=false
     }
 }
